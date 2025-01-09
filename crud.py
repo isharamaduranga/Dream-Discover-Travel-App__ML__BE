11d70451548a718fc427dd80b72e1e666833f33c
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2025-01-09 14:59:35 +0530

    create new function for image upload in aws

---
+++ crud.py
@@ -0,0 +1,42 @@
+# crud.py
+import os
+
+import boto3
+from botocore.exceptions import NoCredentialsError
+from dotenv import load_dotenv
+from fastapi import UploadFile, Form
+from passlib.context import CryptContext
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# Function to hash a password
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+def upload_to_aws(file, bucket, s3_file, acl="public-read"):
+    print(f"Uploading {s3_file} to {bucket}")
+
+    s3 = boto3.client('s3', aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+                      aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+                      region_name=os.environ.get("REGION_NAME"))
+    try:
+        # Ensure the file cursor is at the beginning before uploading
+        file.seek(0)
+        s3.upload_fileobj(file, bucket, s3_file, ExtraArgs={'ACL': acl})
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+
+
+
